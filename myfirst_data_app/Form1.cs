87afa09901949aad9fe7089249f29b5b4181f020
jim96388 <jim96388@gmail.com>
2021-12-12 16:35:42 +0800

    test
    test_button

--- myfirst_data_app/Form1.cs
+++ myfirst_data_app/Form1.cs
@@ -16,5 +16,10 @@ namespace myfirst_data_app
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
